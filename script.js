1108297403b951fa04e9c8d11ca2fb20c4a32d08
joshua <joshuamulinge570@gmail.com>
2026-05-07 23:38:49 +0300

    final-project

--- script.js
+++ script.js
@@ -1,111 +1,83 @@
-// --- Logic For All Pages ---
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Dynamic Date Display (DOM Manipulation)
-    const dateDisplay = document.getElementById('date-display');
-    if (dateDisplay) {
-        dateDisplay.innerText = `Today is ${new Date().toLocaleDateString()}`;
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  // Dynamic Date Display (DOM Manipulation)
+  const dateDisplay = document.getElementById("date-display");
+  if (dateDisplay) {
+    dateDisplay.innerText = `Today is ${new Date().toLocaleDateString()}`;
+  }
 });
 
-//--- Logic for INSERT COURSE PAGE ---
-const courseForm = document.getElementById('course-form');
+// Logic for insert course page
+const courseForm = document.getElementById("course-form");
 if (courseForm) {
-    courseForm.addEventListener('submit', (e) => {
-      e.preventDefault(); //Prevent Page Reload
+  courseForm.addEventListener("submit", (e) => {
+    e.preventDefault(); //Prevent Page Reload
 
-      //Get Values
-      const name = document.getElementById("courseName").value;
-      const instructor = document.getElementById("instructor").value;
-        const nameError = document.getElementById("nameError");
-        const successMessage = document.getElementById('success-message');
+    //Get Values
+    const name = document.getElementById("courseName").value;
+    const instructor = document.getElementById("instructor").value;
+    const nameError = document.getElementById("nameError");
+    const successMessage = document.getElementById("success-message");
 
-      //VALIDATION CHECK
-      if (name.length < 3) {
-        nameError.innerText = "Course Name is too short!";
-        return;
-      }
+    // Validation check to see if name is too short
+    if (name.length < 3) {
+      nameError.innerText = "Course Name is too short!";
+      return;
+    }
 
-      //Persistence: Retrieve existing data or create empty array
-      const courses = JSON.parse(localStorage.getItem("courses")) || [];
+    // Get data from local storage
+    const courses = JSON.parse(localStorage.getItem("courses")) || [];
 
-      //Add New Object
-        courses.push({ name, instructor });
-        
-       // Save back to local Storage
-      localStorage.setItem("courses", JSON.stringify(courses));
+    // Add New Data
+    courses.push({ name, instructor });
 
-        courseForm.reset();
-        
-        //UI Feedback
-        if (successMessage) {
-            successMessage.style.display = 'block';
-        }
-      alert("Course saved!");
-    });
+    // Save back to local Storage
+    localStorage.setItem("courses", JSON.stringify(courses));
+
+    courseForm.reset();
+
+    // Alert messages
+    if (successMessage) {
+      successMessage.style.display = "block";
+    }
+    alert("Course saved!");
+  });
 }
 
-
-
-
-
-//-- Logic for Dashboard )Index) Page ---
-const container = document.getElementById('course-list-container');
+// Logic for Dashboard Page
+const container = document.getElementById("course-list-container");
 if (container) {
-    const storedCourses = JSON.parse(localStorage.getItem('courses')) || [];
+  const storedCourses = JSON.parse(localStorage.getItem("courses")) || [];
 
-
-    //Check if Array is empty
-    if (storedCourses.length === 0) {
-        container.innerHTML = "<p>No courses added yet.Go to insert course'!</p>";
-
-    
-    } else {
-        //Dynamic display using DOM Manipulation
-        container.innerHTML = "";
-        storedCourses.forEach(course => {
-            const card = document.createElement('div');
-            card.className = 'course-card';
-            card.innerHTML = `
-                       <h4>${course.name}</h4>
-                       <p>Instructor:${course.instructor}</p>
+  //Check if Array is empty
+  if (storedCourses.length === 0) {
+    container.innerHTML = "<p>No courses added yet.Go to insert course'!</p>";
+  } else {
+    // If courses are there and are not 0 we render the courses
+    container.innerHTML = "";
+    storedCourses.forEach((course) => {
+      const card = document.createElement("div");
+      card.className = "course-card";
+      card.innerHTML = `
+                       <h4 style="color: blue">${course.name}</h4>
+                       <p style="color: green">Instructor:${course.instructor}</p>
                        `;
-            container.appendChild(card);
-        });
-    }
+      container.appendChild(card);
+    });
+  }
 }
 
-
-
-
-        
-
-
-//--- Logic for About Page ---
-const countDisplay = document.getElementById('course-count');
+// Logic for About Page
+const countDisplay = document.getElementById("course-count");
 if (countDisplay) {
-    //Retrieve Array From local Storage
-    const storedCourses = JSON.parse(localStorage.getItem('courses')) || [];
-    //Dynamically update the DOM with the count of items
-    countDisplay.innerText = storedCourses.length;
-    //BONUS:Add a hover effect via Js event listener as per rubric
-    countDisplay.addEventListener('mouseover', () => {
-        countDisplay.style.color = '#3498db';
-    });
-    countDisplay.addEventListener('mouseout', () => {
-        countDisplay.style.color = '#2c3e50';
-    });
-    }
-    
-
-
-
-
-
-        
-
-
-
-    
-    
-    
+  // Retrieve Array From local Storage
+  const storedCourses = JSON.parse(localStorage.getItem("courses")) || [];
+  // Update the DOM with the count of items
+  countDisplay.innerText = storedCourses.length;
+  // Event listeners to change color on hover
+  countDisplay.addEventListener("mouseover", () => {
+    countDisplay.style.color = "red";
+  });
+  countDisplay.addEventListener("mouseout", () => {
+    countDisplay.style.color = "darkblue";
+  });
+}
